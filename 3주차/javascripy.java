449ab8aa0fd7a574b3d8549d8b048b361e200143
Yondu <68101034+Yondubuntu@users.noreply.github.com>
2022-03-24 18:32:51 +0900

    Add files via upload

---
+++ 3주차/javascripy.java
@@ -0,0 +1,17 @@
+
+public class javascripy {
+	
+	public static void main(String[]args)
+	{ 
+		
+		String s = new String ("Hello world");
+		String t = s.toUpperCase();
+		
+		
+		StringBuilder sb = new StringBuilder("Hello world");
+		
+//		System.out.print(t); 
+		
+	}
+
+}
